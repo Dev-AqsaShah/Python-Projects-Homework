baf2b037495e21bfe64a532a1698d112e94827c4
aqsashah <aqsanor665@gmail.com>
2025-04-03 16:32:10 +0500

    .

---
+++ assign-00-to-05/02-lists/01-add-many-number.py
@@ -0,0 +1,13 @@
+def add_number(number)->int:
+     num:int = 0
+     for i in number:
+         num +- i
+     return num
+    
+def main():
+        numbers:list[int] = [1,2,3,4,5]
+        sum = add_number(numbers)
+        print(sum)
+        
+if __name__ == "__main__":
+        main()
