0c469ae23f0006f42f5b1e7986c26dfa27fdb06b
aqsashah <aqsanor665@gmail.com>
2025-04-12 22:32:26 +0500

    .

---
+++ assign-00-to-05/03-if-statements/02-interrational-voting-age.py
@@ -0,0 +1,18 @@
+peturksbouipo : int = 16
+stanlv:int = 25
+mayengwa:int = 48
+
+def main():
+    age:int = int(input("how old are you"))
+    if age >= peturksbouipo:
+        print(f"your age is {age}. you are eligible to vote in paturksbuipo")
+    else:
+        print{"you are eligible to vote in stanlv"}
+    if age >= stanlv:
+        print(f"your age is {age}. you are eligible to vote in stanlv")
+    else:
+        print{f"your age is {age} you are eligible to vote in mayengwa"}
+    if age >= stanlv:
+        print(f"your age is {age}. you are not eligible to vote in stanlv")
+    else:
+        print(f"your age is {age} you are not eligible to vote")
