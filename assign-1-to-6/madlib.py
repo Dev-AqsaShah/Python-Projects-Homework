0e2f2a6709262f530dc9dcb4c3a97d99aa08fdee
aqsashah <aqsanor665@gmail.com>
2025-05-01 16:02:55 +0500

    done

--- assign-1-to-6/madlib.py
+++ assign-1-to-6/madlib.py
@@ -4,4 +4,23 @@ def mad_lib():
     name = input("give me a name: ")
     place = input("give me a place:")
     funny_adj = input("give me a funny adjective:")
+    random_object = input("give me a random object: ")
+    animal = input("give me an animal:")
+    action_verb = input("give me an action verb:")
+    funny_exclamation = input("give me a funny exclamation:")
+    
+    story = f"""
+    once upon a time, there was a person named {name} who lived in {place}.
+    one day, they found a {funny_adj} {random_object} that belonged to a {animal}.
+    the {animal} was very upset and started to {action_verb} around 
+    name couldn't help but lough and shouted "{funny_exclamation}".
+    """
+    
+    print("\n here is your mad libs story:")
+    
+    if __name__ == "__main__":
+        mad_lib()
+    
+    
+    
     
