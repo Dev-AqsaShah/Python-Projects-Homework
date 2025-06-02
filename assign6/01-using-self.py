65c2846c6171dcf5d28448885789ac0e9a5d0486
aqsashah <aqsanor665@gmail.com>
2025-06-02 21:01:38 +0500

    .

--- assign6/01-using-self.py
+++ assign6/01-using-self.py
@@ -1,3 +1,9 @@
+# 1. Using self
+# Assignment:
+# Create a class Student with attributes name and marks. Use the self keyword to initialize these values via a constructor. Add a method display() that prints student details.
+
+
+
 class Student:
     def __init__(self,name,marks):
         self.name = name
@@ -7,7 +13,7 @@ class Student:
         print(f'Name: {self.name}, Marks: {self.marks}')
         
 Student1:Student = Student("AQSA shah", 95)
-Student2:Student = Student("Mosin shah", 85)
+Student2:Student = Student("unknown", 85)
 print("Student 1 Details:",Student1.display())
 print(Student1.name,Student1.marks)
 print("Student 2 Details:", Student2.display())
