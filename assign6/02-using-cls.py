8f50d81951f1dec1364fe7974cf05141e8fe5502
aqsashah <aqsanor665@gmail.com>
2025-06-03 20:09:28 +0500

    Update 02-using-cls.py

--- assign6/02-using-cls.py
+++ assign6/02-using-cls.py
@@ -3,3 +3,17 @@
 # Create a class Counter that keeps track of how many objects have been created. Use a class variable and a class method with cls to manage and display the count.
 
 
+class Counter:
+    count = 0
+    
+    def __init__(self):
+        Counter.count += 1
+        
+        @classmethod
+        def display_count(cls):
+            return f'Total objects created: {cls.count}'
+        
+    obj1 : Counter = Counter()  # noqa: F821
+    obj2 : Counter = Counter()  # noqa: F821
+    obj3 : Counter = Counter()  # noqa: F821
+    print(Counter.display_count())
