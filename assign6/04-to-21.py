6a282b9bfbc6c8593dfec5282c4c35580459ca3f
aqsashah <aqsanor665@gmail.com>
2025-06-05 12:24:23 +0500

    Update 04-to-21.py

--- assign6/04-to-21.py
+++ assign6/04-to-21.py
@@ -46,3 +46,26 @@ class Logger:
         
 log:Logger = Logger()
 del log
+
+
+# 7. Access Modifiers: Public, Private, and Protected
+# Assignment:
+# Create a class Employee with:
+
+# a public variable name,
+
+# a protected variable _salary, and
+
+# a private variable __ssn.
+
+# Try accessing all three variables from an object of the class and document what happens.
+
+
+class Employee:
+    def __init__(self,name,salary,ssn):
+        self.name = name
+        self._salary = salary
+        self.__ssn = ssn
+        
+emp1 : Employee = Employee("aqsa shah",50000,"123-45-678")
+print(emp1.name) 
